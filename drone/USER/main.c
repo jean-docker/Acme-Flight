17fff3a8b7d0138115a1178c594dc2032931163f
‘blx <1423296426@qq.com>
2019-08-19 01:40:36 +0800

    完成ADC多通道转换逻辑

--- drone/USER/main.c
+++ drone/USER/main.c
@@ -6,7 +6,13 @@ void main (void)
 	GPIO_Init();
 	PWM_Init();
 	ADC_Init();
-	UART_Init();
-	while(1);
+	UART0_Init();
+	
+	while(1)
+	{
+		//ADC_Start_Cvt();
+		Send_Data_To_UART0(0x1e);
+		Timer0_Delay1ms(1);
+	}
 }
 
